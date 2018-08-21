b2e6fe9782e053636ab04c456cb75e918567440d
Leigh <lpyeh@umich.edu>
2018-08-21 10:17:37 -0700

    Small changes to run file and first test run

--- run.sh
+++ run.sh
@@ -4,9 +4,10 @@
 PYTH_PATH="python3"
 echo "Using python located in: ${PYTH_PATH}"
 
-alias python= PYTH_PATH
 
-export WORKING_DIR=/home/aida/neural_profiles_datadir
+#alias python=$PYTH_PATH
+
+export WORKING_DIR=/Users/leigh/Desktop/neural_profiles_datadir
 export PROJ_NAME="MFQ-facebook"
 export INSTANCE_NAME="test_run"
 
@@ -15,15 +16,15 @@ python get_params.py
 . set-vars.sh $WORKING_DIR $PROJ_NAME $INSTANCE_NAME
 
 echo "Processing data for project $PROJ_NAME"
-#./prep_data.sh
+./prep_data.sh
 
 echo "Generating features"
-#./gen_features.sh
+./gen_features.sh
 
 echo "Building and Training baseline methods using generated/supplied features"
-#./train_baselines.sh
+./train_baselines.sh
 
 echo "Building and Training neural network"
-./train_neural.sh
+#./train_neural.sh
 
 #./evaluate.sh
