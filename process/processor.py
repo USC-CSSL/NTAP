efa0b0a981e60e455c5ac755ab66ae8003277dba
neetikasinghal <neetikasinghal16@gmail.com>
2019-02-27 14:06:51 -0800

    updated tagme function with 4 threads.

--- process/processor.py
+++ process/processor.py
@@ -1,11 +1,18 @@
-import re, os, json, sys, emoji
-import pandas as pd
-from progressbar import ProgressBar
-from nltk.stem import PorterStemmer
-from stanfordcorenlp import StanfordCoreNLP
+import emoji
+import json
+import os
+import re
+
 import requests
-from time import sleep
-corenlp_path = os.environ("CORENLP")
+from progressbar import ProgressBar
+from stanfordcorenlp import StanfordCoreNLP
+
+corenlp_path = os.environ.get("CORENLP")
+import pandas as pd
+import threading
+from queue import Queue
+from dask.diagnostics import ProgressBar
+import time
 
 link_re = re.compile(r"http(s)?[^\s]*")
 # link_re = re.compile(r"(http?://[^\s]*)")
@@ -20,7 +27,8 @@ patterns = {"mentions":     mention_re,
             #"emojis":       emoji_re,
          #   "emoticons":    emoticon_re
            }
-
+print_lock = threading.Lock()
+queue = Queue()
 class Preprocessor:
     def __init__(self, dest_dir):
         self.dest = dest_dir
@@ -106,7 +114,8 @@ class Preprocessor:
     def depparse(self):
         print("Not Implemented (dependency parsing)")
 
-    def tagme(self, token, p=0.1):
+    #tagme version 1
+    def tagme_v1(self, token, p=0.1):
         #NOTES: 
         #       - If parallelizing, limit to 4 concurrent calls
         #       - Pause (10s) periodically on each thread
@@ -138,7 +147,7 @@ class Preprocessor:
             for doc in pbar(self.data[self.text_col].values):
                 count += 1
                 if count % 200 == 0:
-                    sleep(5)  # sleep for 5 seconds
+                    # sleep(5)  # sleep for 5 seconds
                     print("Processed {} docs".format(count))
                     abstract_file.flush()
                     category_file.flush()
@@ -174,6 +183,95 @@ class Preprocessor:
             saved_data.to_pickle(os.path.join(entity_dir, "saved.pkl"))
             return
         self.data.loc[:, "tagme_entities"] = pd.Series(extracted, index=self.data.index)
+
+    def tagme_helper(self, doc, request_str, params, abstract_file, category_file, entities, extracted, p):
+        row = list()
+        params["text"] = doc
+        res = requests.get(request_str, params=params)
+        if res.status_code == 200:
+            try:
+                ann = res.json()["annotations"]
+                filt = [entry for entry in ann if entry["link_probability"] > p]
+                for filtered in filt:
+                    if filtered["id"] not in entities:
+                        entities[filtered["id"]] = filtered["title"]
+                        # write entity to file with abstract
+                        abstract_file.write("{}\t{}\t{}".format(
+                            filtered["id"], filtered["title"],
+                            filtered["abstract"].replace('\t', '')))
+                        abstract_file.write('\n')
+                        for cat in filtered["dbpedia_categories"]:
+                            category_file.write("{}\t{}".format(filtered["id"], cat))
+                            category_file.write("\n")
+                    # write entity info to list, to save in self.data
+                    row.append((filtered["id"], filtered["start"], filtered["end"], filtered["link_probability"]))
+            except Exception as e:
+                pass
+        else:
+            row.append(("No Response",))
+        if row:
+            extracted.append(row)
+        return extracted
+
+    def threader(self, request_str, params, abstract_file, category_file, entities, extracted, p, entity_dir):
+        while True:
+            # gets a worker from the queue
+            worker = queue.get()
+
+            # Run the example job with the avail worker in queue (thread)
+            self.tagme_helper(worker, request_str, params, abstract_file, category_file, entities, extracted, p)
+
+            #print thread name and doc
+            with print_lock:
+                print(threading.current_thread().name, worker)
+
+            # completed with the job
+            queue.task_done()
+
+    # tagme version 2
+    def tagme(self, token, p=0.1):
+        entity_dir = os.path.join(self.dest, "tagme")
+        if not os.path.isdir(entity_dir):
+            os.makedirs(entity_dir)
+        entities = dict()
+        extracted = list()
+        params = {'gcube-token': token,
+                  'lang': 'en',
+                  'include_abstract': 'true',
+                  'include_categories': 'true'
+                 }
+        abstract_file = open(os.path.join(entity_dir, "abstracts.tsv"), 'a+')
+        abstract_file.seek(0)
+        category_file = open(os.path.join(entity_dir, "categories.tsv"), 'a+')
+        for line in abstract_file:
+            if len(line.strip()) > 0:
+                id_, title, _ = line.split('\t')
+                entities[id_] = title
+
+        request_str = "https://tagme.d4science.org/tagme/tag"
+
+        start = time.time()
+        for doc in self.data[self.text_col].values:
+            queue.put(doc)
+
+        #intializing 4 threads
+        try:
+            num_of_threads = 4
+            for x in range(num_of_threads):
+                t = threading.Thread(target=self.threader, args = (request_str, params, abstract_file, category_file, entities, extracted, p, entity_dir))
+                t.daemon = True
+                t.start()
+            queue.join()
+
+        except KeyboardInterrupt:
+            partial_index = list(self.data.index)[:len(extracted)]
+            saved_data = pd.Series(extracted, index=partial_index)
+            print("Interrupted; saved to file")
+            saved_data.to_pickle(os.path.join(entity_dir, "saved.pkl"))
+            return
+        self.data.loc[:, "tagme_entities"] = pd.Series(extracted)
+        print('Time taken by tagme job(sec):', time.time() - start)
+
 
     def write(self, formatting='.json'):
         """
