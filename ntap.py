53f463ebff8b54135fc5c7fabf32f5d1415db818
neetikasinghal <neetikasinghal16@gmail.com>
2019-04-17 14:36:39 -0700

    refactoring ntap and methods

--- ntap.py
+++ ntap.py
@@ -5,13 +5,10 @@ import json
 import os
 from baselines.features import Features
 from baselines.methods import Baseline
-
-from methods.neural.nn import *
-from methods.neural.neural import Neural
 import pandas as pd
-import pickle
-import numpy as np
-from collections import Counter
+
+from run_methods import Methods
+
 
 class Ntap:
 
@@ -93,105 +90,11 @@ class Ntap:
             baseline_pipeline.load_method(method)
             baseline_pipeline.go()
 
-    def __get_text_col(self, cols):
-        print("...".join(cols))
-        notvalid = True
-        while notvalid:
-            text_col = input("Enter text col from those above: ")
-            if text_col.strip() not in cols:
-                print("Not a valid column name")
-            else:
-                notvalid = False
-        return text_col
-
-    def __get_target_col(self, cols):
-        notvalid = True
-        while notvalid:
-            target_col = input("Enter target cols you want to train the model on, from those above: ")
-            targets = target_col.strip().replace(" ", "").split(",")
-            notvalid = False
-            for target in targets:
-                if target not in cols:
-                    notvalid = True
-                    print("Not a valid column name")
-        return targets
-
-    def __get_pred_col(self, cols):
-        notvalid = True
-        while notvalid:
-            target_col = input("Enter target cols you want to predict, from those above: ")
-            targets = target_col.strip().replace(" ", "").split(",")
-            notvalid = False
-            for target in targets:
-                if target not in cols:
-                    notvalid = True
-                    print("Not a valid column name")
-        return targets
-
-    def run_method(self, params):
-        missing_indices = list()
-        weights = dict()
-        neural_params = params['neural_params']
-        # TODO: Add exception handling
-        text_col = "text"  # __get_text_col(train_data.columns.tolist())
-        neural_params["target_cols"] = self.__get_target_col(self.data.columns.tolist())
-        # params["pred_cols"] = __get_pred_col(train_data.columns.tolist())
-        for target in neural_params["target_cols"]:
-            print("Removing missing values from", target, "column")
-            missing_indices.extend(self.data[self.data[target] == -1.].index)
-            print("Statistics of", target)
-            count_pre = Counter(self.data[target].tolist())
-            count = sorted(count_pre.items())
-            count = list(dict(count).values())
-            weights[target] = np.array([(sum(count) - c) / sum(count) for c in count])
-        train_data = self.data.drop(missing_indices)
-        train_data = train_data.dropna(subset=[text_col])
-        print("Shape of train_dataframe after getting rid of the Nan values is", train_data.shape)
-
-        train_data = tokenize_data(train_data, text_col, neural_params["max_length"], neural_params["min_length"])
-        text = train_data[text_col].values.tolist()
-
-        print(len(text), "text data remains in the dataset")
-        vocab = learn_vocab(text, neural_params["vocab_size"])
-        # vocab_size = len(vocab)
-        # method_class = load_method(method_string)
-
-        X = np.array(tokens_to_ids(text, vocab))
-        y = np.transpose(np.array([np.array(train_data[target].astype(int)) for target in neural_params["target_cols"]]))
-        # y = np.transpose(np.array(np.array(train_data[params["target_cols"]].astype(int))))
-        feature = params['model']['feature']
-        feature_file = fnmatch.filter(os.listdir(self.feature_dir), feature + '.*')[0]
-        feature_path = os.path.join(self.feature_dir, feature_file)
-        if neural_params["model"][-4:] == "feat":
-            if feature_path.endswith('.tsv'):
-                feat = pd.read_csv(feature_path, sep='\t', quoting=3).values
-            elif feature_path.endswith('.pkl'):
-                feat = pickle.load(open(feature_path, 'rb')).values
-            elif feature_path.endswith('.csv'):
-                feat1 = pd.read_csv(feature_path)
-                feat = feat1.loc[:, ["care", "harm", "fairness", "cheating"]].values
-            neural_params["feature_size"] = feat.shape[1]
-        else:
-            feat = []
-        neural = Neural(params, vocab)
-        neural.build()
-
-        if neural_params["train"]:
-            if neural_params["kfolds"] <= 1:
-                raise Exception('Please set the parameter, kfolds greater than 1')
-            neural.cv_model(X, y, weights, self.model_path, feat)
-
-        if neural_params["predict"]:
-            if self.test_filepath is None:
-                raise Exception("Please specify the path to the data to be predicted")
-            all = self.load_preprocessed_data(self.test_filepath)
-
-            col = self.__get_text_col(all.columns.tolist())
-            all = tokenize_data(all, col, neural_params["max_length"], neural_params["min_length"])
-            all_text = all[col].values.tolist()
-            data = np.array(tokens_to_ids(all_text, vocab))
-
-            neural.run_model(X, y, data, weights, self.model_path)
+    def run(self):
+        method = Methods()
+        feature_file = os.path.join(self.feature_dir, params['model']['feature'] + '.tsv')
+        method.run_method(params['model']['method'], self.data, params['neural_params'], self.test_filepath,
+                          self.model_path, feature_file)
 
 
 if __name__ == '__main__':
@@ -203,4 +106,4 @@ if __name__ == '__main__':
     else:
         ntap.data = ntap.load_preprocessed_data(ntap.preprocessed_file)
     ntap.baseline()
-    ntap.run_method(params)
+    ntap.run()
---
+++ run_methods.py
@@ -0,0 +1,170 @@
+#TODO: Add CNN and RNN models to files cnns.py and rnns.py
+from methods.neural.nn import *
+from methods.neural.neural import Neural
+import pandas as pd
+import pickle
+import argparse
+import numpy as np
+from collections import Counter
+import json
+
+
+"""
+def load_method(method_string):
+    method_map = {"CNN": CNN}  # TODO: Add others
+    if method_string not in method_map:
+        raise ValueError("Invalid method string ({})".format(method_string))
+        exit(1)
+    return method_map[method_string]
+"""
+class Methods:
+    def __get_text_col(self, cols):
+        print("...".join(cols))
+        notvalid = True
+        while notvalid:
+            text_col = input("Enter text col from those above: ")
+            if text_col.strip() not in cols:
+                print("Not a valid column name")
+            else:
+                notvalid = False
+        return text_col
+
+
+    def __get_target_col(self, cols):
+        notvalid = True
+        while notvalid:
+            target_col = input("Enter target cols you want to train the model on, from those above: ")
+            targets = target_col.strip().replace(" ", "").split(",")
+            notvalid = False
+            for target in targets:
+                if target not in cols:
+                    notvalid = True
+                    print("Not a valid column name")
+        return targets
+
+    def __get_pred_col(self, cols):
+        notvalid = True
+        while notvalid:
+            target_col = input("Enter target cols you want to predict, from those above: ")
+            targets = target_col.strip().replace(" ", "").split(",")
+            notvalid = False
+            for target in targets:
+                if target not in cols:
+                    notvalid = True
+                    print("Not a valid column name")
+        return targets
+
+
+    def run_method(self, method_string, train_data, params, data, save, features):
+        missing_indices = list()
+        weights = dict()
+
+        # TODO: Add exception handling
+        text_col = "text"#__get_text_col(train_data.columns.tolist())
+        params["target_cols"] = self.__get_target_col(train_data.columns.tolist())
+        #params["pred_cols"] = __get_pred_col(train_data.columns.tolist())
+        for target in params["target_cols"]:
+            print("Removing missing values from", target, "column")
+            missing_indices.extend(train_data[train_data[target] == -1.].index)
+            print("Statistics of", target)
+            count_pre = Counter(train_data[target].tolist())
+            count = sorted(count_pre.items())
+            count = list(dict(count).values())
+            weights[target] = np.array([(sum(count) - c) / sum(count) for c in count])
+        train_data = train_data.drop(missing_indices)
+        train_data = train_data.dropna(subset=[text_col])
+        print("Shape of train_dataframe after getting rid of the Nan values is", train_data.shape)
+
+        train_data = tokenize_data(train_data, text_col, params["max_length"], params["min_length"])
+        text = train_data[text_col].values.tolist()
+
+        print(len(text), "text data remains in the dataset")
+        vocab = learn_vocab(text, params["vocab_size"])
+        # vocab_size = len(vocab)
+        # method_class = load_method(method_string)
+
+        X = np.array(tokens_to_ids(text, vocab))
+        y = np.transpose(np.array([np.array(train_data[target].astype(int)) for target in params["target_cols"]]))
+        # y = np.transpose(np.array(np.array(train_data[params["target_cols"]].astype(int))))
+        if params["model"][-4:] == "feat":
+            if features.endswith('.tsv'):
+                feat = pd.read_csv(features, sep='\t', quoting=3).values
+            elif features.endswith('.pkl'):
+                feat = pickle.load(open(features, 'rb')).values
+            elif features.endswith('.csv'):
+                feat1  = pd.read_csv(features)
+                feat = feat1.loc[:,["care", "harm", "fairness", "cheating"]].values
+            params["feature_size"] = feat.shape[1]
+        else:
+            feat = []
+        with open('params.json') as f:
+            all_params = json.load(f)
+        neural = Neural(all_params, vocab)
+        neural.build()
+
+        if params["train"]:
+            if params["kfolds"]<=1:
+                raise Exception('Please set the parameter, kfolds greater than 1')
+            neural.cv_model(X, y, weights, save, feat)
+
+        if params["predict"]:
+            if data is None:
+                raise Exception("Please specify the path to the data to be predicted")
+            if data.endswith('.tsv'):
+                all = pd.read_csv(data, sep='\t', quoting=3)
+            elif data.endswith('.pkl'):
+                all = pickle.load(open(data, 'rb'))
+            elif data.endswith('.csv'):
+                all = pd.read_csv(data)
+
+            col = self.__get_text_col(all.columns.tolist())
+            all = tokenize_data(all, col, params["max_length"], params["min_length"])
+            all_text = all[col].values.tolist()
+            data = np.array(tokens_to_ids(all_text, vocab))
+
+            neural.run_model(X, y, data, weights, save)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train_data", help="Path to train data; includes text and any additional \
+                        train_data columns, such as POS")
+    parser.add_argument("--data", help="Path to data to be labeled")
+    parser.add_argument("--method", help="Method string; see README for complete list")
+    parser.add_argument("--params", help="Path to parameter file; can be .txt, .json, .csv")
+    parser.add_argument("--savedir", help="Directory to save results in")
+    parser.add_argument("--feature", help="Path to the features file")
+
+    args = parser.parse_args()
+
+    if args.train_data.endswith('.tsv'):
+        train_data = pd.read_csv(args.train_data, sep='\t', quoting=3)
+    elif args.train_data.endswith('.pkl'):
+        train_data = pd.read_pickle(args.train_data)
+    elif args.train_data.endswith('.csv'):
+        train_data = pd.read_csv(args.train_data)
+
+    with open('params.json') as f:
+        params = json.load(f)
+
+    data = args.data
+    method = args.method
+    params = params['neural_params']
+    features = args.feature
+    save = args.savedir
+    """
+    params = args.params
+    with open(params, 'r') as fo:
+        if params.endswith(".txt"):
+            params = params.read()
+        elif params.endswith(".json"):
+            params = json.load(fo)
+        elif params.endswith(".csv"):
+            params = pd.read_csv(params)
+        else:
+            raise ValueError("Error: Incorrect extension for parameter file, must be a .txt, .json, or a .csv")
+    """
+    method = Methods()
+    method.run_method(method, train_data, params, data, save, features)
+
+    # call method constructors; build, train (using columns in args.train_data), and generate predictions
+
